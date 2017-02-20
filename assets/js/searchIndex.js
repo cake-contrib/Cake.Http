ff00eec5f63ee52357fa9e60f2fff6f4739c3c1a
Gary Ewan Park <gep13@gep13.co.uk>
2017-02-20 22:54:10 +0000

    AppVeyor Publish: 430fe4858aa124a403f533a5a2b5718d72c7c913 (GH-3) Added Wyam Documentation

---
+++ assets/js/searchIndex.js
@@ -0,0 +1,98 @@
+
+var camelCaseTokenizer = function (obj) {
+    var previous = '';
+    return obj.toString().trim().split(/[\s\-]+|(?=[A-Z])/).reduce(function(acc, cur) {
+        var current = cur.toLowerCase();
+        if(acc.length === 0) {
+            previous = current;
+            return acc.concat(current);
+        }
+        previous = previous.concat(current);
+        return acc.concat([current, previous]);
+    }, []);
+}
+lunr.tokenizer.registerFunction(camelCaseTokenizer, 'camelCaseTokenizer')
+var searchModule = function() {
+    var idMap = [];
+    function y(e) { 
+        idMap.push(e); 
+    }
+    var idx = lunr(function() {
+        this.field('title', { boost: 10 });
+        this.field('content');
+        this.field('description', { boost: 5 });
+        this.field('tags', { boost: 50 });
+        this.ref('id');
+        this.tokenizer(camelCaseTokenizer);
+
+        this.pipeline.remove(lunr.stopWordFilter);
+        this.pipeline.remove(lunr.stemmer);
+    });
+    function a(e) { 
+        idx.add(e); 
+    }
+
+    a({
+        id:0,
+        title:"HttpSettingsExtensions",
+        content:"HttpSettingsExtensions",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:1,
+        title:"HttpSettings",
+        content:"HttpSettings",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:2,
+        title:"HttpClientAliases",
+        content:"HttpClientAliases",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:3,
+        title:"CakeHttpClientHandler",
+        content:"CakeHttpClientHandler",
+        description:'',
+        tags:''
+    });
+
+    y({
+        url:'/Cake.Http/Cake.Http/api/Cake.Http/HttpSettingsExtensions',
+        title:"HttpSettingsExtensions",
+        description:""
+    });
+
+    y({
+        url:'/Cake.Http/Cake.Http/api/Cake.Http/HttpSettings',
+        title:"HttpSettings",
+        description:""
+    });
+
+    y({
+        url:'/Cake.Http/Cake.Http/api/Cake.Http/HttpClientAliases',
+        title:"HttpClientAliases",
+        description:""
+    });
+
+    y({
+        url:'/Cake.Http/Cake.Http/api/Cake.Http/CakeHttpClientHandler',
+        title:"CakeHttpClientHandler",
+        description:""
+    });
+
+    return {
+        search: function(q) {
+            return idx.search(q).map(function(i) {
+                return idMap[i.ref];
+            });
+        }
+    };
+}();
